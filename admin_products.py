72e8f189337cf02e24b48d13d4dadbff2fb2fae5
Zakhar Rokishchuk <zakhar.rokishchuk@pandadoc.com>
2022-04-25 19:59:55 +0400

    ended refactoring of admin_products

--- admin_products.py
+++ admin_products.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, redirect, session, request
-from data_manipulations import get_products, save_products
+from flask import Blueprint, render_template, redirect, request, current_app
+from data_manipulations import filter_products_by_type, get_products, save_products, filter_products_by_type, resize_new_product_picture, get_product, save_product
 from werkzeug.utils import secure_filename
 import os
-from PIL import Image
+
 
 admin_products = Blueprint('admin_products', __name__,
                            template_folder='templates')
@@ -10,16 +10,11 @@ admin_products = Blueprint('admin_products', __name__,
 
 @admin_products.route('/admin/products')
 def admin_products_list():
-    applied_filter = request.args.get("product_filter_form")
-    filtered_products = []
     products = get_products()
-    if applied_filter:
-        for product in products:
-            if product["type"] == applied_filter:
-                filtered_products.append(product)
-        return render_template('products.html', items=filtered_products, applied_filter=applied_filter)
-    save_products(products)
-    return render_template('products.html', items=products, applied_filter=applied_filter)
+    product_type = request.args.get("product_filter")
+    if request.args.get("product_filter"):
+        return render_template('products.html', items=filter_products_by_type(product_type), applied_filter=product_type)
+    return render_template('products.html', items=products, applied_filter=product_type)
 
 
 @admin_products.route('/admin/product/adding_product')
@@ -28,73 +23,52 @@ def adding_product():
 
 
 @admin_products.route('/admin/product/adding_product/save', methods=['POST'])
-def save_adding_product():
+def save_added_product():
     products = get_products()
-    if request.method == "POST":
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(
-            admin_products.config['UPLOAD_FOLDER'], filename))
-        session.modified = True
-        new_product = {}
-        new_product["name"] = request.form["name"]
-        new_product["price"] = int(request.form["price"])
-        new_product["description"] = request.form["description"]
-        new_product["short_description"] = request.form["short_description"]
-        new_product["type"] = request.form["type"]
-        new_product["id"] = products[-1]['id'] + 1
-        new_product["img_src"] = filename
-        new_product["display"] = "Off"
-        new_product["quantity"] = 1
-        new_product["storage_quantity"] = int(request.form["storage_quantity"])
-        products.append(new_product)
-        save_products(products)
-    image_path = Image.open("static/img/"+filename)
-    new_image_163 = image_path.resize((163, 163))
-    new_image_333 = image_path.resize((333, 333))
-    new_image_555 = image_path.resize((555, 555))
-    new_image_163.save("static/img/163/163_"+filename)
-    new_image_333.save("static/img/333/333_"+filename)
-    new_image_555.save("static/img/555/555_"+filename)
-    os.remove("static/img/"+filename)
+    new_product = {}
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+    resize_new_product_picture(filename)
+    new_product["name"] = request.form["name"]
+    new_product["price"] = int(request.form["price"])
+    new_product["description"] = request.form["description"]
+    new_product["short_description"] = request.form["short_description"]
+    new_product["type"] = request.form["type"]
+    new_product["id"] = products[-1]['id'] + 1
+    new_product["img_src"] = filename
+    new_product["display"] = "Off"
+    new_product["quantity"] = 1
+    new_product["storage_quantity"] = int(request.form["storage_quantity"])
+    products.append(new_product)
+    save_products(products)
     return redirect("/admin/products")
 
 
 @admin_products.route('/admin/products/<int:product_id>/edit')
 def editing_page(product_id):
-    products = get_products()
-    product = next(
-        product for product in products if product['id'] == product_id)
-    return render_template('editing_product.html', product=product)
+    return render_template('editing_product.html', product=get_product(product_id))
 
 
 @admin_products.route('/admin/products/edit/<int:product_id>/save', methods=['POST'])
 def product_save(product_id):
-    products = get_products()
-    if request.method == "POST":
-        session.modified = True
-        product = next(
-            product for product in products if product['id'] == product_id)
-        product['name'] = request.form["name"]
-        product['price'] = int(request.form["price"])
-        product['description'] = request.form["description"]
-        product['short_description'] = request.form["short_description"]
-        product['display'] = request.form["display"]
-        product['img_src'] = request.form["img_src"]
-        product['type'] = request.form["type"]
-        product["quantity"] = 1
-        product['storage_quantity'] = int(request.form["storage_quantity"])
-        save_products(products)
+    product = get_product(product_id)
+    product['name'] = request.form["name"]
+    product['price'] = int(request.form["price"])
+    product['description'] = request.form["description"]
+    product['short_description'] = request.form["short_description"]
+    product['display'] = request.form["display"]
+    product['img_src'] = request.form["img_src"]
+    product['type'] = request.form["type"]
+    product["quantity"] = 1
+    product['storage_quantity'] = int(request.form["storage_quantity"])
+    save_product(product)
     return redirect("/admin/products")
 
 
 @admin_products.route('/admin/products/<int:product_id>/display', methods=['POST'])
 def change_display(product_id):
-    products = get_products()
-    if request.method == "POST":
-        session.modified = True
-        product_to_display = next(
-            product for product in products if product['id'] == product_id)
-        product_to_display["display"] = request.form["if_display"]
-        save_products(products)
+    product = get_product(product_id)
+    product["display"] = request.form["if_display"]
+    save_product(product)
     return redirect("/admin/products")
--- data_manipulations.py
+++ data_manipulations.py
@@ -1,6 +1,9 @@
+from itertools import product
 from flask import request
 import json
 from time_record import get_date_time, get_time_unix
+from PIL import Image
+import os
 
 
 def get_products():
@@ -38,12 +41,12 @@ def save_order(order_to_save):
     order = next(i for i in orders if i['id'] == order_to_save['id'])
     if 'comment' in order:
         order['comment'] = order_to_save['comment']
-    order['name'] = order_to_save["name"]
-    order['address'] = order_to_save["address"]
-    order['phone'] = order_to_save["phone"]
-    order['payment_method'] = order_to_save["payment_method"]
-    order["status"] = order_to_save["status"]
-    order['items'] = order_to_save["items"]
+    order['name'] = order_to_save['name']
+    order['address'] = order_to_save['address']
+    order['phone'] = order_to_save['phone']
+    order['payment_method'] = order_to_save['payment_method']
+    order['status'] = order_to_save['status']
+    order['items'] = order_to_save['items']
     save_orders(orders)
 
 
@@ -51,6 +54,21 @@ def get_product(product_id):
     products = get_products()
     product = next(i for i in products if i['id'] == product_id)
     return product
+
+
+def save_product(product_to_save):
+    products = get_products()
+    product = next(i for i in products if i['id'] == product_to_save['id'])
+    product['name'] = product_to_save['name']
+    product['price'] = product_to_save['price']
+    product['description'] = product_to_save['description']
+    product['short_description'] = product_to_save['short_description']
+    product['display'] = product_to_save['display']
+    product['img_src'] = product_to_save['img_src']
+    product['type'] = product_to_save['type']
+    product['quantity'] = product_to_save['quantity']
+    product['storage_quantity'] = product_to_save['storage_quantity']
+    save_products(products)
 
 
 def get_products_to_display():
@@ -128,3 +146,23 @@ def add_comment_to_order(order_id):
     order = next(i for i in orders if i['id'] == order_id)
     order["comment"] = request.form["comment"]
     save_orders(orders)
+
+
+def filter_products_by_type(type):
+    products = get_products()
+    filtered_products = []
+    for product in products:
+            if product["type"] == type:
+                filtered_products.append(product)
+    return filtered_products
+
+
+def resize_new_product_picture(filename):
+    image_path = Image.open("static/img/"+filename)
+    new_image_163 = image_path.resize((163, 163))
+    new_image_333 = image_path.resize((333, 333))
+    new_image_555 = image_path.resize((555, 555))
+    new_image_163.save("static/img/163/163_"+filename)
+    new_image_333.save("static/img/333/333_"+filename)
+    new_image_555.save("static/img/555/555_"+filename)
+    os.remove("static/img/"+filename)
